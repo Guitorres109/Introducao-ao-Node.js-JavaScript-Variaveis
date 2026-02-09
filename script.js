4b7407a66d5c11c36edaf5615bc9eb2f98cf1838
Guilherme <torres.guib2009@gmail.com>
2026-02-09 16:04:08 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,106 @@
+//Desafio 1
+//--------------------------------------------------------------------
+
+let meuNome = "Guilherme";
+let minhaIdade = "16"
+
+console.log(" ")
+console.log("Olá, meu nome é "+meuNome+" e minha idade é "+minhaIdade)
+console.log(" ")
+
+//--------------------------------------------------------------------
+//Desafio 2
+//--------------------------------------------------------------------
+
+let comidaFavorita = "Parmediana de carne";
+ comidaFavorita = "Strogonoff";
+
+const paisdeNascimento = "Brasil";
+// paisdeNascimento = "Argentina"
+
+//--------------------------------------------------------------------
+//Desafio 3
+//--------------------------------------------------------------------
+
+let Nome = "Guilherme";
+let idade = "16"
+let cidade = "Guarulhos"
+console.log(" ")
+console.log("Olá meu nome é "+ Nome + ", tenho "+ idade + " anos e moro em "+ cidade)
+console.log(" ")
+
+//--------------------------------------------------------------------
+//Desafio 4
+//--------------------------------------------------------------------
+
+let curso = "Desenvolvimento de Sistemas"
+console.log("")
+console.log("Meu curso é "+curso)
+console.log(" ")
+
+curso = "Mecatrônica"
+console.log(" ")
+console.log("Agora meu curso é "+curso)
+
+//--------------------------------------------------------------------
+//Desafio 5
+//--------------------------------------------------------------------
+
+const anoAtual = "2026";
+// anoAtual = "2027"
+
+//--------------------------------------------------------------------
+//Desafio 6
+//--------------------------------------------------------------------
+
+let nome2 = "Guilherme"
+let sobrenome = "Torres"
+console.log(" ")
+console.log("Meu nome completo é "+nome2+" "+sobrenome)
+
+//--------------------------------------------------------------------
+//Desafio 7
+//--------------------------------------------------------------------
+
+let usuario = "guibtorres"
+let email = "torres.guib2009@gmail.com"
+
+console.log(" ")
+console.log("-----------------------------------------------------")
+console.log("Usuario: "+usuario)
+console.log("Email: "+email)
+console.log("Idade: "+idade)
+console.log("-----------------------------------------------------")
+console.log(" ")
+
+//--------------------------------------------------------------------
+//Desafio 8
+//--------------------------------------------------------------------
+
+let n1 = 10;
+let n2 = 20;
+console.log(" ")
+console.log("Numero 1: "+n1+", Numero 2: "+n2)
+console.log(" ")
+
+//--------------------------------------------------------------------
+//Desafio 9
+//--------------------------------------------------------------------
+
+let nota1 = 10;
+let nota2 = 7.0;
+let nota3 = 8.5;
+let media = (nota1+nota2+nota3)/3;
+console.log(" ")
+console.log("Notas: "+nota1+", "+nota2+" e "+nota3)
+console.log("A média do aluno é "+ media)
+console.log(" ")
+
+//--------------------------------------------------------------------
+//Desafio 10
+//--------------------------------------------------------------------
+
+let produto = "Fiat Freemont 2013";
+let preco = "R$ 49.990"
+console.log(" ");
+console.log(produto + " por " + preco)
